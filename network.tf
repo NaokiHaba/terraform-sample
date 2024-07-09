01595e98a9b0a71577a2ec8e0a0a8c2ccc05173b
Naoki Haba <naosprintrunner6385@gmail.com>
2024-07-10 02:10:22 +0900

    feat: VPC、サブネット、ルートテーブル、インターネットゲートウェイ追加、マップ公開 IP を false に変更

--- network.tf
+++ network.tf
@@ -1,3 +1,6 @@
+# ------------------------------
+# VPC
+# ------------------------------
 resource "aws_vpc" "vpc" {
   cidr_block                       = "192.168.0.0/20"
   instance_tenancy                 = "default"
@@ -12,6 +15,10 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+# ------------------------------
+# subnets
+# VPC をさらに区切ったネットワーク
+# ------------------------------
 resource "aws_subnet" "public_subnet_1a" {
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = "ap-northeast-1a"
@@ -42,7 +49,7 @@ resource "aws_subnet" "private_subnet_1a" {
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = "ap-northeast-1a"
   cidr_block              = "192.168.3.0/24"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name    = "${var.project}-${var.environment}-private-subnet-1a"
@@ -55,11 +62,81 @@ resource "aws_subnet" "private_subnet_1c" {
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = "ap-northeast-1c"
   cidr_block              = "192.168.4.0/24"
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name    = "${var.project}-${var.environment}-private-subnet-1c"
     Project = var.project
     Env     = var.environment
   }
+}
+
+# ------------------------------
+# Route Table
+# public_subnet_1a・public_subnet_1c からのトラフィックはpublic_route_tableのルートを通る
+# private_subnet_1a・private_subnet_1c からのトラフィックはprivate_route_tableのルートを通る
+# ------------------------------
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-public-route-table"
+    Project = var.project
+    Env     = var.environment
+    Type    = "public"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_1a" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet_1a.id
+}
+
+resource "aws_route_table_association" "public_rt_1c" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet_1c.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-route-table"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_1a" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private_subnet_1a.id
+}
+
+resource "aws_route_table_association" "private_rt_1c" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id      = aws_subnet.private_subnet_1c.id
+}
+
+# ------------------------------
+# Internet Gateway
+# ------------------------------
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-igw"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+# ------------------------------
+# Route
+# ------------------------------
+
+resource "aws_route" "public_route_igw-route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  gateway_id             = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
 }
